a9154d24e916a6a323eae47977eef22a49410f01
abdulhadi291 <abdulhadikamal291@gmail.com>
2025-08-31 16:14:20 +0500

    javascript basics

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log("hello world");
+
+if (true) {
+  let a=12, b=12, c=6;
+  const d = 5,
+    e = 1;
+  var f = 15;
+let g= a+b;
+  console.log(g);
+}
+
+for(var i=2;i<=20;i=i+2){
+    console.log(`${i}`)
+}
+console.log(`Value of F is ${f}` );
+
+function greet(){
+console.log("Welcome");
+}
+greet();
+
+const person= {
+name : "Abdul Hadi",
+age  : 19,
+occupation: "student"
+}
+console.log(`${person.age}`);
