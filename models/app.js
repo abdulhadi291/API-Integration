1ed0852354871d08c27840b360c0c1ff956facb7
abdulhadi291 <abdulhadikamal291@gmail.com>
2025-09-07 16:39:41 +0500

    Mongo DB Records Creation

--- models/app.js
+++ models/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const User = require("./users")
+const User = require("../users")
 const app = express();
 app.use(express.json());
 
